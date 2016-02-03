a7769ec9ce21402c512f84b94d90cc0281294e32
Zane Durkin <zanedurkin@gmail.com>
2016-02-02 19:57:36 -0700

    first push

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cookie_monster</title>
+    </head>
+    <body>
+<?php
+
+if(array_key_exists("Red_Guy's_name", $_COOKIE)&&preg_match('/([Ee])(lmo)+/', $_COOKIE["Red_Guy's_name"])){
+  echo '<p>You got it! Flag: #omm\nom\nom</p>';
+}else{
+    echo("<p>He's my favorite red guy</p>");
+    setcookie("Red_Guy's_name", 'Name', time()+300);
+}
+
+?>
+</body>
+</html>
