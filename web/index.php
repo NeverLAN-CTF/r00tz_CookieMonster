2352cd640d3a2321f4c784cbc7b483d857259734
Zane Durkin <zanedurkin@gmail.com>
2016-07-14 12:19:30 -0600

    adding keyword

--- web/index.php
+++ web/index.php
@@ -7,7 +7,8 @@
 <?php
 
 if(array_key_exists("Red_Guy's_name", $_COOKIE)&&preg_match('/([Ee])(lmo)+/', $_COOKIE["Red_Guy's_name"])){
-  echo '<p>You got it! Flag: #omm\nom\nom</p>';
+  echo('<p>You got it! Flag: #omm\nom\nom</p>');
+  echo('<p>WORD: Go</p>');
 }else{
     echo("<p>He's my favorite red guy</p>");
     setcookie("Red_Guy's_name", 'Name', time()+300);
