e3ce1ddd1301006a58194db643898798b932d4d4
Dan Daggett <dan@socialgeeks.com>
2016-08-03 04:29:55 +0000

    Change WORD to "Go to".

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
 
 if(array_key_exists("Red_Guy's_name", $_COOKIE)&&preg_match('/([Ee])(lmo)+/', $_COOKIE["Red_Guy's_name"])){
   echo('<p>You got it! Flag: #omm\nom\nom</p>');
-  echo('<p>WORD: Go</p>');
+  echo('<p>WORD: Go to</p>');
 }else{
     echo("<p>He's my favorite red guy</p>");
     setcookie("Red_Guy's_name", 'Name', time()+300);
